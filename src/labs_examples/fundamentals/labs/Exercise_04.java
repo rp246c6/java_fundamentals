b804efb3e73702d660494e5fbb164215b8481311
Roopam <rp246c6@gmail.com>
2026-02-27 20:26:35 +0530

    added files Exercise_04.txt, Exercise_05.java

--- src/labs_examples/fundamentals/labs/Exercise_04.java
+++ src/labs_examples/fundamentals/labs/Exercise_04.java
@@ -16,6 +16,26 @@ public class Exercise_04 {
         System.out.println("int i is: " + i);
 
         // write your code below
+        boolean value = true;
+        System.out.println("boolean value is : " + value);
+
+        char c = 'c';
+        System.out.println("char c is : " + c);
+
+        byte b = 125;
+        System.out.println("byte b is : " + b);
+
+        short s = 1000;
+        System.out.println("short s is : " + s);
+
+        long l = 6752426542642242L;
+        System.out.println("long l is : " + l);
+
+        float f = 3.14f;
+        System.out.println("long f is : " + f);
+
+        double d = 65445643453.675435354354;
+        System.out.println("double d is : " + d);
 
     }
 
--- src/labs_examples/fundamentals/labs/Exercise_05.java
+++ src/labs_examples/fundamentals/labs/Exercise_05.java
@@ -1,6 +1,8 @@
 package labs_examples.fundamentals.labs;
 
 
+import java.sql.SQLOutput;
+
 /**
  * Fundamentals Exercise 5: Working with Strings
  *
@@ -14,14 +16,37 @@ public class Exercise_05 {
 
         String str = "hello!";
         // please declare an int variable below, and set it to the value of the length of "str"
+        int i = str.length();
 
         String str2 = "hello";
         // please initialize a boolean variable and test whether str is equal to str2
+        boolean isEqual = str.equals(str2) ;
+
+        System.out.println("Are the strings equal? " + isEqual);
 
         // please concatenate str & str2 and set the result to a new String variable below
+        String result = str + str2;
+        System.out.println(result);
 
         // please demonstrate the use of any other method that is available to us in the String class
         // for example, replace(), substring(), contains(), indexOf() etc
+
+        // 1. replace(): Replace characters or substrings
+        String replaced = str.replace("hello!", "Java");
+        System.out.println("After replace: " + replaced); // Java
+
+        // 2. substring(): Extract part of the string
+        String sub = str.substring(0, 3);
+        System.out.println("Substring: " + sub); // hel
+
+        // 3. contains(): Check if a substring exists
+        boolean hasO = str.contains("o");
+        System.out.println("Contains 'o'? " + hasO); // true
+
+        // 4. indexOf(): Find the position of a character or substring
+        int index = str.indexOf("o");
+        System.out.println("First index of 'o': " + index); // 4
+
 
     }
 
