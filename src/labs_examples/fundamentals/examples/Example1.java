4f61a26f10febf45f491dc139c99d40e89b26bda
Roopam <rp246c6@gmail.com>
2026-02-27 18:07:10 +0530

    added files Example1.java, Example2.java, Exercise_01.txt, Exercise_02.txt, Exercise_03.java

--- src/labs_examples/fundamentals/examples/Example1.java
+++ src/labs_examples/fundamentals/examples/Example1.java
@@ -2,11 +2,12 @@ package labs_examples.fundamentals.examples;
 
 class Example1 {
     // A Java program begins with a call to main().
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         System.out.println("Hello World! Hello World!!");
         int next = 10;
         int y = 7;
-        System.out.println(next + y);
+        int result = next + y;
+        System.out.println(result);
     }
 }
